48ea72f5794314d632e404cf3418f866557ca2b8
Wersiy Odilous <wersiy.odilous@gmail.com>
2023-11-14 16:30:47 +0100

    my first commit for this repo

---
+++ httpd_roles/install_role/files/script_install.sh
@@ -0,0 +1,3 @@
+        #!/bin/bash
+        rm -rf /var/www/html/* 
+        cp -rf /home/ec2-user/httpd/medlife-master/* /var/www/html/
---
+++ httpd_roles/uninstall_role/files/script_uninstall.sh
@@ -0,0 +1,4 @@
+        #!/bin/bash
+        rm -rf /var/www/html/*
+        rm -rf /home/ec2-user/httpd
+	rm -rf /tmp/httpd/*
